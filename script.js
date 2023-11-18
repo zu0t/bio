e2424a2f9cf969201a9686fd38ab103770ea2ef7
zuot <150614447+zu0t@users.noreply.github.com>
2023-11-18 23:40:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,87 @@
 function isMobileDevice() {
-            return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-        }
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
-        function redirectToMobileSite() {
-            window.location.href = "https://m.zuot.cc/";
-        }
+function redirectToMobileSite() {
+    window.location.href = "https://m.zuot.cc/";
+}
 
-        if (isMobileDevice()) {
-            redirectToMobileSite();
-		
-        }
-	function typeText(element, text, speed) {
-        let index = 0;
-        const timer = setInterval(function() {
-          if (index < text.length) {
+if (isMobileDevice()) {
+    redirectToMobileSite();
+}
+
+function typeText(element, text, speed) {
+    let index = 0;
+    const timer = setInterval(function () {
+        if (index < text.length) {
             element.textContent += text[index];
             index++;
-          } else {
+        } else {
             clearInterval(timer);
-          }
-        }, speed);
-      }
-      function animateText() {
-        const titleElement = document.querySelector(".title");
-        const subtitleElement = document.querySelector(".podtitle");
+        }
+    }, speed);
+}
 
-        typeText(titleElement, "zuot", 800);
-        setTimeout(function() {
-          typeText(subtitleElement, "Hello, I am zuot and I live in Czechia.", 80);
-        }, 4000);
-      }
+function animateText() {
+    const titleElement = document.querySelector(".title");
+    const subtitleElement = document.querySelector(".podtitle");
 
-      document.addEventListener("contextmenu", function(e) {
+    typeText(titleElement, "zuot", 800);
+    setTimeout(function () {
+        typeText(subtitleElement, "Hello, I am zuot and I live in Czechia.", 80);
+    }, 4000);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("load", animateText);
+});
+
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+});
+
+window.addEventListener("keydown", function (e) {
+    if ((e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I") || (e.ctrlKey && e.shiftKey && e.key === "J"))) {
         e.preventDefault();
-      });
+    }
+});
 
-      window.addEventListener("keydown", function(e) {
-    	if ((e.key === "F12" || (e.ctrlKey && e.shiftKey && e.key === "I")) || (e.ctrlKey && e.shiftKey && e.key === "J")) {
-	  e.preventDefault();
-    	}
-      });
-
-      document.addEventListener("keydown", function(e) {
-        if ((e.ctrlKey && e.shiftKey && e.key === 'C') || (e.ctrlKey && e.shiftKey && e.key === 'c')) {
-          e.preventDefault();
-        }
-      });
-
-      document.addEventListener("keydown", function(e) {
-        if ((e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'c')) {
-          e.preventDefault();
-        }
-      });
-
-      document.addEventListener("keydown", function(e) {
-        if ((e.ctrlKey && e.shiftKey && e.key === 'J') || (e.ctrlKey && e.shiftKey && e.key === 'c')) {
-          e.preventDefault();
-        }
-      });
-	    
-      window.addEventListener("load", animateText);
-      document.addEventListener("contextmenu", function(e) {
+document.addEventListener("keydown", function (e) {
+    if ((e.ctrlKey && e.shiftKey && (e.key === 'C' || e.key === 'I' || e.key === 'J'))) {
         e.preventDefault();
-      });
+    }
+});
 
-      document.addEventListener("keydown", function(e) {
-        if ((e.ctrlKey && e.shiftKey && e.key === 'C') || (e.ctrlKey && e.shiftKey && e.key === 'c')) {
-          e.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+    const title = document.querySelector('.title');
+
+    function toggleCase() {
+        const text = title.textContent;
+        let modifiedText = '';
+
+        for (let i = 0; i < text.length; i++) {
+            const isUppercase = Math.random() < 0.5;
+            modifiedText += isUppercase ? text[i].toUpperCase() : text[i].toLowerCase();
         }
-      });
-      
-      document.addEventListener("keydown", function(e) {
-        if ((e.ctrlKey && e.shiftKey && e.key === 'I') || (e.ctrlKey && e.shiftKey && e.key === 'c')) {
-          e.preventDefault();
-        }
-      });
-      
-      document.addEventListener("keydown", function(e) {
-        if ((e.ctrlKey && e.shiftKey && e.key === 'J') || (e.ctrlKey && e.shiftKey && e.key === 'c')) {
-          e.preventDefault();
-        }
-      });
-      document.addEventListener('DOMContentLoaded', function () {
-            const title = document.querySelector('.title');
 
-            function toggleCase() {
-                const text = title.textContent;
-                let modifiedText = '';
+        title.textContent = modifiedText;
 
-                for (let i = 0; i < text.length; i++) {
-                    const isUppercase = Math.random() < 0.5;
-                    modifiedText += isUppercase ? text[i].toUpperCase() : text[i].toLowerCase();
-                }
+        // Náhodné rozhodnutí pro Bold, underline, italic, strikethrough
+        const style = [
+            Math.random() < 0.5 ? 'bold' : 'normal',
+            Math.random() < 0.5 ? 'underline' : 'none',
+            Math.random() < 0.5 ? 'italic' : 'normal',
+            Math.random() < 0.5 ? 'line-through' : 'none'
+        ];
 
-                title.textContent = modifiedText;
+        title.style.fontWeight = style.includes('bold') ? 'bold' : 'normal';
+        title.style.textDecoration = style.includes('underline') ? 'underline' : 'none';
+        title.style.fontStyle = style.includes('italic') ? 'italic' : 'normal';
+        title.style.textDecorationLine = style.includes('line-through') ? 'line-through' : 'none';
+    }
 
-                // Náhodné rozhodnutí pro Bold, underline, italic, strikethrough
-                const style = [
-                    Math.random() < 0.5 ? 'bold' : 'normal',
-                    Math.random() < 0.5 ? 'underline' : 'none',
-                    Math.random() < 0.5 ? 'italic' : 'normal',
-                    Math.random() < 0.5 ? 'line-through' : 'none'
-                ];
-
-                title.style.fontWeight = style.includes('bold') ? 'bold' : 'normal';
-                title.style.textDecoration = style.includes('underline') ? 'underline' : 'none';
-                title.style.fontStyle = style.includes('italic') ? 'italic' : 'normal';
-                title.style.textDecorationLine = style.includes('line-through') ? 'line-through' : 'none';
-            }
-
-            setInterval(toggleCase, 150); // Střídání každé 2 sekundy
-        });
+    setInterval(toggleCase, 150); // Střídání každé 2 sekundy
+});
 
 const asciiArt = `
 ███████╗██╗   ██╗ ██████╗ ████████╗
